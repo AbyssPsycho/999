9a69456fa1cbd7d0b7c9dcefda1968ed9b95d05a
AbyssPsycho <81321973+AbyssPsycho@users.noreply.github.com>
2022-05-12 09:44:15 -0400

    Added Ability To Undo

--- js/tile.js
+++ js/tile.js
@@ -1,7 +1,7 @@
 function Tile(position, value) {
   this.x                = position.x;
   this.y                = position.y;
-  this.value            = value || 3;
+  this.value            = value || 2;
 
   this.previousPosition = null;
   this.mergedFrom       = null; // Tracks tiles that merged together
@@ -16,12 +16,16 @@ Tile.prototype.updatePosition = function (position) {
   this.y = position.y;
 };
 
-Tile.prototype.serialize = function () {
-  return {
-    position: {
-      x: this.x,
-      y: this.y
-    },
-    value: this.value
-  };
-};
+Tile.prototype.save = function (next) {
+  var copy = {}
+  copy.x = this.x;
+  copy.y = this.y;
+  copy.value = this.value;
+  copy.previousPosition = {
+    // In order to reverse the animation, we store the
+    // next position as the previous
+    x: next.x,
+    y: next.y
+  }
+  return copy;
+}
